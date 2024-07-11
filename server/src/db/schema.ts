cf698bfe05516fdcf0c4a0db557d87613957032a
thinh tu <khangthinhproc@gmail.com>
2024-07-11 13:23:50 +0700

    add DrizzleORM to the database

---
+++ server/src/db/schema.ts
@@ -0,0 +1,7 @@
+import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).notNull().unique(),
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import dotenv from "dotenv";
+
+import {client} from './db/database'
+
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
