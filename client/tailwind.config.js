ed9175398b72ae7f984759d26a852fa4320369c6
thinh tu <khangthinhproc@gmail.com>
2024-07-16 12:29:37 +0700

    configure Flowbite for UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
+const flowbite = require('flowbite-react/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
     extend: {
       fontFamily: {
@@ -12,5 +11,5 @@ export default {
       },
     },
   },
-  plugins: [],
-}
+  plugins: [flowbite.plugin()],
+};
