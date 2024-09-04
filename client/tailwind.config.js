0e6bc693327282a8007aa1fcfc70b0a6453636df
thinh tu <khangthinhproc@gmail.com>
2024-09-04 20:35:06 +0700

    font fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,11 +4,10 @@ const flowbite = require('flowbite-react/tailwind');
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-        slab: ['Roboto Slab', 'serif'],
-      },
+    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      slab: ['Roboto Slab', 'serif'],
     },
   },
   plugins: [flowbite.plugin()],
