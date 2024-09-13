ff51ae12a9a675a7577d68f4ca994fa2a3ed6e71
thinh tu <khangthinhproc@gmail.com>
2024-09-13 14:31:37 +0700

    reconfigure enviroment variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './');
+
+  return {
+    server: {
+      define: {
+        __APP_MODE__: JSON.stringify(env.VITE_APP_MODE),
+        __API_URL__: JSON.stringify(env.VITE_API_URL),
+      },
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: mode === 'development' ? true : false,
+        },
       },
     },
-  },
-  plugins: [react()],
+    plugins: [react()],
+  };
 });
