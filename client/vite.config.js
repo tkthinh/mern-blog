ecb99a9c04b1668e06d42df5fd54cda0a59eac9c
thinh tu <khangthinhproc@gmail.com>
2024-09-13 15:13:37 +0700

    reconfigure enviroment variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
         '/api': {
           target: env.VITE_API_URL,
           changeOrigin: true,
-          secure: mode === 'development' ? true : false,
+          secure: mode === 'development' ? false : true,
         },
       },
     },
